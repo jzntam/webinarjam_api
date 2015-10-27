bef53dee724e05f43777b2e87fd6eed45f673dfb
Jason Tam <jason@wishpond.com>
2015-10-26 17:33:35 -0700

    Add get_webinars_method

--- lib/webinarjam_api.rb
+++ lib/webinarjam_api.rb
@@ -1,5 +1,17 @@
 require "webinarjam_api/version"
+require "httparty"
 
 module WebinarjamApi
-  # Your code goes here...
+  class Client
+    API_ENDPOINT = "https://app.webinarjam.com/api/v2"
+
+    def initialize(api_key)
+      @api_key = api_key
+    end
+
+    def get_webinars
+      data     = { api_key: @api_key }
+      response = HTTParty.post("#{API_ENDPOINT}/webinars", body: data )
+    end
+  end
 end
