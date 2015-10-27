1efd9a98fef419149b6a69a4174c094cedd46816
Jason Tam <jason@wishpond.com>
2015-10-27 11:18:29 -0700

    Add get_webinar and register_person methods

--- lib/webinarjam_api.rb
+++ lib/webinarjam_api.rb
@@ -13,5 +13,16 @@ module WebinarjamApi
       data     = { api_key: @api_key }
       response = HTTParty.post("#{API_ENDPOINT}/webinars", body: data )
     end
+
+    def get_webinar(webinar_id)
+      data     = { api_key: @api_key, webinar_id: webinar_id }
+      response = HTTParty.post("#{API_ENDPOINT}/webinar", body: data )
+    end
+
+    def register_person(webinar_id, schedule, lead)
+      data      = { api_key: @api_key, webinar_id: webinar_id, schedule: schedule }
+      lead_data = data.merge(lead)
+      response  = HTTParty.post("#{API_ENDPOINT}/register", body: lead_data )
+    end
   end
 end
